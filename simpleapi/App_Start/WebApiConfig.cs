fa0d8b2f40366166e472db240535982c0a4f0764
M. A. Martín Hrdez <mookiefumi@outlook.com>
2016-11-27 11:24:38 +0100

    Initial commit

---
+++ simpleapi/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+using simpleapi.infrastructure;
+
+namespace simpleapi
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			config.Formatters.Clear();
+			config.Formatters.Add(new JsonMediaTypeFormatter());
+			//config.Formatters.Add(new XmlMediaTypeFormatter());
+
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			//Handlers
+			config.MessageHandlers.Add(new RequireHttpsMessageHandler());
+		}
+	}
+
+
+}
---
+++ simpleapi/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace simpleapi.models
+{
+	public class Product
+	{
+		public int Id
+		{
+			get;
+			set;
+		}
+		public string Name
+		{
+			get;
+			set;
+		}
+		public string Category
+		{
+			get;
+			set;
+		}
+		public decimal Price
+		{
+			get;
+			set;
+		}
+	}
+}
